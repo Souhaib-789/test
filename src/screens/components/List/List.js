6d901020a869e4627b4d6367a9c952ff6d211ec4
Souhaib-789 <souhaibsouhaib838@gmail.com>
2024-03-02 00:00:46 +0500

    s

--- src/screens/components/List/List.js
+++ src/screens/components/List/List.js
@@ -1,19 +1,33 @@
 import React from "react";
 import styles from ".//List.module.css";
-import Card from "../Card/Card";
+// import Card from "../Card/Card";
+import { Card, List } from 'antd';
 
-const List = (props) => {
+const Listx = (props) => {
     return (
         <div className={styles.row}>
-            {
+            {/* {
                 props?.options?.map((item , index) => {
                     return (
                         <Card item={item} index={index} />
                     );
                 })
-            }
+            } */}
+
+            <List
+                grid={{
+                    gutter: 16,
+                    column: 4,
+                }}
+                dataSource={props?.options}
+                renderItem={(item) => (
+                    <List.Item>
+                        <Card title={item.title}>Card content</Card>
+                    </List.Item>
+                )}
+            />
         </div>
     );
 }
 
-export default List;
+export default Listx;
